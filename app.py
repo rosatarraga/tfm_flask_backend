e7276e49e907197872dba4a054c9ce4d3b5d5ed5
rosatarraga <rouxtm11@gmail.com>
2022-06-08 18:08:22 +0200

    Updated new log when using app.

--- app.py
+++ app.py
@@ -1,36 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
-from dbBreastCancer import Log
+from dbBreastCancer import Logs
 from datetime import date
 import json
+import base64
+from io import BytesIO
+from PIL import Image
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/dbbreastcancer'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
 db = SQLAlchemy(app)
+
+    #data = json.dumps({ 'data': request.get_json().get('email') })
+    #url = data["email"]
+    #print(request.json())
 
 @app.route('/uploadImage', methods=['POST'])
 def uploadImage():
     # get url
     print("alguien te llama")
-    data = json.loads(request.data.decode())
-    url = data["email"]
-    print(url)
+    data = request.form
+    email = data['email']
+    url = data['image']
+    logUser(email)
+    print(email)
+    image_name = url[-10:-4] + ".png"
+    if "data:image/png;base64," in url:
+        base_string = url.replace("data:image/png;base64,", "")
+
+    elif "data:image/jpeg;base64," in url:
+        base_string = url.replace("data:image/jpeg;base64,", "")
+    
+    decoded_img = base64.b64decode(base_string)
+    img = Image.open(BytesIO(decoded_img))
+
+    file_name = image_name
+    img.save(file_name, "png")
+
+    print("completado")
     # start job
     
     # return created job id
     return 'ok'
 
-@app.route('/home', methods=['GET'])
-def home():
+def logUser(email):
     today = date.today()
     # dd/mm/YY
     d1 = today.strftime("%d/%m/%Y")
-    log = Log('Rosita', d1)
+    log = Logs(email, d1)
     log.save()
-    return render_template('index.html')
 
 @app.route('/breastcancer', methods=['GET'])
 def breastcancer():
--- dbBreastCancer.py
+++ dbBreastCancer.py
@@ -4,20 +4,19 @@ from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/dbbreastcancer'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://db.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret string'
 
 db = SQLAlchemy(app)
 
-
-class Log(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10000), unique=False, nullable=False)
+class Logs(db.Model):
+    id = db.Column(db.Integer, db.Identity(start=1, cycle=True), primary_key=True)
+    email = db.Column(db.String(10000), unique=False, nullable=False)
     date = db.Column(db.DateTime(timezone=True), server_default=func.now())
-
-    def __init__(self, name, date):
-        print (name,date)
-        self.name = name
+    print (email,date)
+    def __init__(self, email, date):
+        self.email = email
         self.date = date
     def save(self):
         if not self.id:
